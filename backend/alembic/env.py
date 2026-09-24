6c7f716a347d9c6032eff4d91ae55001ebdce8f0
Mohammad Esmail Aligholi <105414292+Mmdia6@users.noreply.github.com>
2026-09-24 11:17:53 +0330

    feat: connect Alembic to model metadata

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,13 +1,14 @@
 from logging.config import fileConfig
 from alembic import context
 from app.core.config import settings
+from app.db.session import Base
+from app import models
 
 config = context.config
 config.set_main_option("sqlalchemy.url", settings.database_url)
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
-
-target_metadata = None
+target_metadata = Base.metadata
 
 def run_migrations_offline() -> None:
     context.configure(url=settings.database_url, target_metadata=target_metadata, literal_binds=True, dialect_opts={"paramstyle": "named"})
